8ab4a9eb82cd5af94db44f28b1d1bacc38ded632
68011128_Sahapoom Rattanawiwatpong <68011128@kmitl.ac.th>
2026-01-16 10:43:22 +0700

    up4

---
+++ src/practical/p04/index.ts
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+export async function countCommentsByPost() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
+    const comments = response.data;
+
+    if (!Array.isArray(comments) || comments.length === 0) {
+      return {};
+    }
+
+    const result = comments.reduce((acc, comment) => {
+      const postId = comment.postId;
+      if (postId === null || postId === undefined) {
+        return acc;
+      }
+      if (acc[postId]) {
+        acc[postId] += 1;
+      } else {
+        acc[postId] = 1;
+      }
+      return acc;
+    }, {});
+
+    return result;
+  } catch (error) {
+    return {};
+  }
+}
